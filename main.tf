7cf3f35a9d997707aa5872bf372e755cdda397dc
kshitij.jo <kshitij.jo@media.net>
2023-12-04 20:24:32 +0530

    lxd vm resource tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    lxd = {
+      source = "terraform-lxd/lxd"
+    }
+  }
+}
+
+provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+}
+
+variable "instance_names" {
+  type = list(string)
+  default = [
+    "kube-control-1",
+    "kube-control-2",
+    "kube-control-3",
+    "kube-minion-1",
+    "kube-minion-2",
+    "kube-minion-3",
+    "kube-lb-1",
+  ]
+}
+
+resource "lxd_instance" "kube" {
+  count     = length(var.instance_names)
+  name      = var.instance_names[count.index]
+  image     = "ubuntu:22.04"
+  type      = "virtual-machine"
+  ephemeral = false
+
+  config = {
+    "boot.autostart" = true
+  }
+
+  limits = {
+    cpu    = 2
+    memory = "3GB"
+  }
+
+  profiles = ["access"]
+}
